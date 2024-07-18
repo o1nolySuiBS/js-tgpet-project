2dae656359dad62cd369ad4aab3b8ccac6f0c2d0
ihor1 <ihor1006@gmail.com>
2024-07-18 03:41:08 +0200

    1

---
+++ index.js
@@ -0,0 +1,30 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '6853339273:AAE7_4pVIPkCXaLeYdK48jikOD-f05OpWAc'
+const webAppUrl = 'https://silver-cheesecake-436994.netlify.app/';
+const bot = new TelegramBot(token, {polling: true})
+
+bot.on('message', async(msg)=>{
+    const chatId = msg.chat.id
+    const text = msg.text;
+
+
+    if(text === '/start'){
+        await bot.sendMessage(chatId, 'Нижче появиться кнопка, заповни форму', {
+            reply_markup:{
+                keyboard:[
+                    [{text: 'Заповнити форму'}]
+                ]
+            }
+        })
+
+    await bot.sendMessage(chatId, 'Це магазин, роби замовлення', {
+            reply_markup:{
+                inline_keyboard: [
+                    [{text: "зробити замовлення", web_app:{url: webAppUrl}}]
+                ]
+            }
+        })
+    }
+    bot.sendMessage(chatId, 'Received tour message')
+})
